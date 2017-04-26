391e7c3e78a042b58ba5c9a18be064be69d4c673
GrayOwl <truegrayowl@gmail.com>
2017-04-26 19:49:34 -0400

    app welcome screen logic

--- app/src/main/java/app/connetick/com/connetick/StartActivity.java
+++ app/src/main/java/app/connetick/com/connetick/StartActivity.java
@@ -1,13 +1,29 @@
 package app.connetick.com.connetick;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartActivity extends AppCompatActivity {
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+
+        login = (Button) findViewById(R.id.login_screen_btn);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent toLoginScreenIntent = new Intent(StartActivity.this,Login.class);
+                startActivity(toLoginScreenIntent);
+            }
+        });
     }
 }
