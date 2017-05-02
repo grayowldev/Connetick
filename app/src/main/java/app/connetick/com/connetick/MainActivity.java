7df0f055d3c2b2360c7665b62be60060bd072b9e
GrayOwl <truegrayowl@gmail.com>
2017-05-01 22:20:02 -0400

    made connection to server

--- app/src/main/java/app/connetick/com/connetick/MainActivity.java
+++ app/src/main/java/app/connetick/com/connetick/MainActivity.java
@@ -1,5 +1,6 @@
 package app.connetick.com.connetick;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -36,31 +37,26 @@ public class MainActivity extends AppCompatActivity {
         testBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+/**
                 new Thread(){
                     public void run(){
-                        try {
-                            double radius = Double.parseDouble(editTextView.getText().toString().trim());
 
-                            dataOut.writeDouble(radius);
-                            dataOut.flush();
-
-                            double area = dataIn.readDouble();
-                            areaView.setText("");
-                            areaView.setText("radius is: " + radius + '\n');
-                            areaView.append("area is: " + area + '\n');
-
-                        }
-                        catch (Exception e){
-                            e.printStackTrace();
-                        }
                     }
                 }.start();
+**/
+                double radius = Double.parseDouble(editTextView.getText().toString().trim());
+                NetworkRunner area = new NetworkRunner();
+                area.execute(radius);
+                areaView.setText("");
+                areaView.setText("radius is: " + radius + '\n');
+                areaView.append("area is: " + area + '\n');
+
+
 
 
             }
         });
-
+/**
         try {
             Socket socket = new Socket("192.168.1.3", portNumber);
             System.out.println("local port: " + socket.getLocalPort());
@@ -72,8 +68,62 @@ public class MainActivity extends AppCompatActivity {
             areaView.append(e.toString() + '\n');
 
         }
+ **/
 
     }
+
+    public class NetworkRunner extends AsyncTask<Double,String, Double>{
+
+        @Override
+        protected Double doInBackground(Double... doubles) {
+            try{
+                Socket socket = new Socket("localHost",portNumber);
+                dataIn = new DataInputStream(socket.getInputStream());
+                dataOut = new DataOutputStream(socket.getOutputStream());
+
+                double radius = doubles[0];
+
+                dataOut.writeDouble(radius);
+                dataOut.flush();
+
+                double area = dataIn.readDouble();
+                return area;
+
+            }
+            catch (Exception e){
+
+            }
+            return 9.87;
+        }
+    }
+
+/**
+        @Override
+        protected String doInBackground(Double... doubles) {
+            try {
+                
+                Socket socket = new Socket("192.168.1.3", portNumber);
+                System.out.println("local port: " + socket.getLocalPort());
+                dataIn = new DataInputStream(socket.getInputStream());
+                dataOut = new DataOutputStream(socket.getOutputStream());
+                //double radius = Double.parseDouble(editTextView.getText().toString().trim());
+
+                dataOut.writeDouble(radius);
+                dataOut.flush();
+
+                double area = dataIn.readDouble();
+                //areaView.setText("");
+                //areaView.setText("radius is: " + radius + '\n');
+                //areaView.append("area is: " + area + '\n');
+
+            }
+            catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+
+**/
+
 
 
 }
