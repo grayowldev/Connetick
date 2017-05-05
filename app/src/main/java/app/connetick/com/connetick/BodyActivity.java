a6b5ca35142e0880235b7434bba20ef88bfea33d
Kwasi <truegrayowl@gmail.com>
2017-05-05 10:19:39 -0400

    updates

--- app/src/main/java/app/connetick/com/connetick/BodyActivity.java
+++ app/src/main/java/app/connetick/com/connetick/BodyActivity.java
@@ -1,7 +1,9 @@
 package app.connetick.com.connetick;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class BodyActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class BodyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_body);
+
+        findViewById(R.id.complete_task_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(BodyActivity.this,CompletionFormActivity.class));
+            }
+        });
     }
 }
